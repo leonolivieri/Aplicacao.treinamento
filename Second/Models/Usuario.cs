eabe9116e79bc79dfda0a89e8bbf785a1108a86b
Leonel Feliciano de Oliveira <Leon2010.sp@hotmail.com>
2021-04-21 20:25:40 -0300

    teste

---
+++ Second/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minha_Sgunda_Aplicação.Models
+{
+    [table("Usuario")]
+    public class Usuario
+        {
+        [Display(Name ="Nome")]
+        [Column("Nome do usuário")]
+        public string Nome { get; set; }
+    }
+
+  
+}
---
+++ Second/Models/tableAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Minha_Sgunda_Aplicação.Models
+{
+    internal class tableAttribute : Attribute
+    {
+        private string v;
+
+        public tableAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
